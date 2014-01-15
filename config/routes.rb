37d741e609da6282ef21425ccdd9d76bf56d46b4
Oscar Jasklowski <oscarjasklowski@gmail.com>
2014-01-15 15:33:40 -0800

    Home page route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Quizbowl::Application.routes.draw do
+  root "pages#home"
+  get "credits" => "pages#credits"
+  get "leaderboard" => "pages#leaderboard"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
